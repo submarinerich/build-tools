25fb36b374c839d7c94d0aeabb360bedbffdac5e
Anderson Miller <anderson@submarinerich.com>
2012-07-31 13:31:46 -0400

    adding a database .rb file

---
+++ database.rb
@@ -0,0 +1,43 @@
+
+require 'rubygems'
+require 'date'
+require 'time'
+require 'fileutils'
+
+desc "create new db file to append"
+task :newsql do
+  puts "creating new sql file with the date stamp"
+  d = Time.new
+  timestring = d.strftime("%Y%m%d%H%M%S")
+  filename = "db/"+timestring+"_dbupdate.sql"
+  File.open(filename,File::RDWR|File::CREAT,0755){ |f|
+    f.rewind
+    f.write("-- update to the db file\n")
+    f.write("-- filename: "+filename+"\n\n")
+    f.close
+  }
+end
+
+desc "init database with only the bootstrap.sql"
+task :initdb do
+  report("humanweatherinitdb")
+  sh "psql -h localhost -U postgres --file db/bootstrap.sql"
+end
+
+
+desc "update the database with everything since bootstrap"
+task :updatedb do
+  Dir.foreach("db"){ | x |
+    if x.include? "dbupdate.sql"
+      puts "sh psql -h localhost -U postgres --file "+x
+    end
+  }
+end
+
+desc "fully reset the db to where it should be and delete everything"
+task :resetdb => :initdb do
+  Rake::Task['updatedb'].invoke
+  puts "database reset"
+end
+
+
